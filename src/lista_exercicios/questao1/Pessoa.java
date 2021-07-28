ddc771d44458f8cab0b2a9bdd5e9c43937f1dd36
Tayse Maia <tayse.maia9@gmail.com>
2021-07-28 09:11:05 -0300

    adicionando projetos de polimorfismo

---
+++ src/lista_exercicios/questao1/Pessoa.java
@@ -0,0 +1,24 @@
+package lista_exercicios.questao1;
+
+public class Pessoa {
+
+    private String nome;
+    private String dataDeNascimento;
+    private double altura;
+
+
+
+    public String getNome(){
+            return this.nome;
+        }
+    public void setNome(String nome_){ this.nome = nome_; }
+
+    public String getDataDeNascimento() { return this.dataDeNascimento; }
+    public void setDataDeNascimento(String data){
+            this.dataDeNascimento = data;
+        }
+
+    public int getIdade(String nascimento){
+
+    }
+}
---
+++ src/lista_exercicios/questao1/TestaPessoa.java
@@ -0,0 +1,8 @@
+package lista_exercicios.questao1;
+
+public class TestaPessoa {
+    public static void main(String[] args) {
+        Pessoa pessoa;
+
+    }
+}
